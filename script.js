e82604a93910642e8997d31d17e133f075610e60
vituud2 <overroot0@gmail.com>
2023-07-28 11:06:34 -0300

    28-07 finished v1.0

--- script.js
+++ script.js
@@ -1,52 +1,87 @@
 let codigoPedido = document.querySelector('#codigoPedido');
 let codigoProduto = document.querySelector('#codigoProduto');
 let plusBtn = document.querySelector('.plusBtn');
-
-// let codigoProdutoHTML = document.querySelectorAll('#codigoProduto');
 let table = document.querySelector('#table');
+let pedidoCountContainer = document.querySelector('#pedidoCountContainer');
+let pedidoCount = 1;
+let regex = /[a-zA-Z]/;
 
+function exportToXLSX() {
+    let headers = ["Pedido", "Produto"];
+    let data = [];
+  
+    let tableRows = document.querySelectorAll('.tableStyle tr');
+    for (let i = 0; i < tableRows.length; i++) {
+      let rowData = [];
+      let cells = tableRows[i].querySelectorAll('td');
+      for (let j = 0; j < cells.length; j++) {
+        rowData.push(cells[j].textContent);
+      }
+      data.push(rowData);
+    }
+  
+    let workbook = XLSX.utils.book_new();
+    let worksheet = XLSX.utils.aoa_to_sheet([headers, ...data]);
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Dados");
+  
+    let today = new Date();
+    let fileName = "dados_" + today.toISOString().slice(0, 10) + ".xlsx";
+    XLSX.writeFile(workbook, fileName);
+  }
 
-// plusBtn.addEventListener('click', ()=> {
-//     let codigoProdutoValor = codigoProduto.value;
-// });
-let row = document.createElement('tr');
-
-
-
-codigoPedido.addEventListener('keydown', (e)=>{
-    if (e.key === "Enter" || e.keycode === 13) {
+codigoPedido.addEventListener('keydown', (e) => {
+    if (e.key === "Enter" || e.keyCode === 13) {
         e.preventDefault();
+        row = document.createElement('tr');
+        countCell = document.createElement('td');
+        
+        if (codigoPedido.value.length !=5) {
+            window.alert('Código de Pedido Incorreto !');
+        } else {
+            row.appendChild(countCell);
+            countCell.classList.add('countCell');
+            countCell.textContent = pedidoCount++;
 
-        let cell = document.createElement('td')
-        cell.classList.add('cellPedido');
-        let codigoPedidoValor = codigoPedido.value;
-        if (codigoPedidoValor) {
-            
-            cell.textContent = codigoPedidoValor;
-            table.appendChild(row);
-            row.appendChild(cell);
+            let cell = document.createElement('td');
+            cell.classList.add('cellPedido');
+            let codigoPedidoValor = codigoPedido.value;
+            if (codigoPedidoValor) {
+                cell.textContent = codigoPedidoValor;
+                table.appendChild(row);
+                row.appendChild(cell);
         }
         codigoProduto.focus();
+        }
     }
-    
 });
 
-codigoProduto.addEventListener('keydown', (e)=>{
-    for (let i = 1; i <= codigoProduto.value.length; i++) {
-        if (e.key === 'Enter' || e.keycode === 13) {
-            e.preventDefault();
-            
-            let cell2 = document.createElement('td')
-    
-            let codigoProdutoValor = codigoProduto.value;
+codigoProduto.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        e.preventDefault();
+
+        let cell2 = document.createElement('td');
+        let codigoProdutoValor = codigoProduto.value;
+
+        if (codigoProduto.value.length !=13 || regex.test(codigoProdutoValor) == true) {
+            console.log(codigoProduto.value.length);
+            window.alert('Código de Produto Incorreto !');
+        } else {
             if (codigoProdutoValor) {
                 cell2.textContent = codigoProdutoValor;
                 row.appendChild(cell2);
             }
-            codigoProduto.value = '';
-            codigoProduto.focus();
         }
+        codigoProduto.value = '';
+        codigoProduto.focus();
     }
 });
 
 
+
+
+plusBtn.addEventListener('click', ()=> {
+    codigoPedido.value = '';
+    codigoProduto.value = '';
+    codigoPedido.focus();
+
+});
